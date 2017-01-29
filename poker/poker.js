c656634b31794637cc412fa4048f19819bf13ea6
abuil <antonio.aldana@kellogg.northwestern.edu>
2017-01-29 17:26:34 -0600

    first version

---
+++ poker/poker.js
@@ -0,0 +1,51 @@
+// DO NOT CHANGE THE CODE BETWEEN HERE AND LINE 30
+
+// function to shuffle (randomize the order of) an array
+// e.g. myArray.shuffle()
+Array.prototype.shuffle = function() {
+  var currentIndex = this.length, temporaryValue, randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = this[currentIndex];
+    this[currentIndex] = this[randomIndex];
+    this[randomIndex] = temporaryValue;
+  }
+  return this;
+}
+
+// returns something representing a deck of cards... try it in the console, you'll see
+// e.g. window.getDeck()
+window.getDeck = function() {
+  var ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace'];
+  var suits = ['clubs', 'diamonds', 'hearts', 'spades'];
+  var cards = [];
+  ranks.forEach(function(rank, index) {
+    suits.forEach(function(suit, index) {
+      cards.push(rank + "_of_" + suit);
+    });
+  });
+  return cards;
+}
+// END DO NOT CHANGE CODE
+
+$(function() {
+  // Your code goes here!
+         
+  $(".deal").on("click", function(event) {
+    event.preventDefault();
+    var elementThatWasClicked = $(this);
+    console.log(elementThatWasClicked);
+    var deck_array = window.getDeck();
+    console.log(deck_array);
+    deck_array.shuffle()
+    console.log(deck_array);
+    
+    //http://golearntocode.com/images/cards/ace_of_hearts.png
+    $("#c1").attr("src", "http://golearntocode.com/images/cards/"+deck_array[0]+".png");
+    $("#c2").attr("src", "http://golearntocode.com/images/cards/"+deck_array[1]+".png");
+    $("#c3").attr("src", "http://golearntocode.com/images/cards/"+deck_array[2]+".png");
+    $("#c4").attr("src", "http://golearntocode.com/images/cards/"+deck_array[3]+".png");
+    $("#c5").attr("src", "http://golearntocode.com/images/cards/"+deck_array[4]+".png");
+  })
+})
---
+++ todo/todo.js
@@ -0,0 +1,13 @@
+// This code runs when the page loads
+$(function() {
+
+  $(".remove-btn-item").on("click", function(event) {
+    event.preventDefault();
+    var elementThatWasClicked = $(this);
+    console.log(elementThatWasClicked);
+    console.log(elementThatWasClicked.parent());
+    console.log(elementThatWasClicked.parent().parent());
+    elementThatWasClicked.parent().parent().remove();
+  })
+
+})
